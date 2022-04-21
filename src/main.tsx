fe6f45000ac12864df4ac0eab855f400814b12bc
Daniel Faucette <dfaucett@redhat.com>
2022-04-21 11:58:13 -0400

    ripped out firebase, tailwind, and daisy ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import { App } from "~/components/root/App";
+// import "./index.css";
+// import { App } from "~/components/root/App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <h1>Hello! React is in the hizzy</h1>
   </React.StrictMode>,
   document.getElementById("root")
 );
--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,14 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()]
+  plugins: [react(), tsconfigPaths()],
+  base: "/security/",
+  server: {
+    port: 3000,
+    strictPort: true,
+    hmr: {
+      host: "127.0.0.1",
+      protocol: "ws",
+    },
+  },
 })
